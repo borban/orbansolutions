ca1a5d359bac650c93cb52fffe0c926a7d7aa631
Benjamin Orban <ben@orbanfamily.org>
2023-07-06 12:38:54 -0500

    Removed amplify

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,34 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import TimeTracker from "./TimeTracker";
 import JobEntry from "./JobEntry";
-import { API } from "aws-amplify";
-import { listJobEntries } from "../graphql/queries";
 
 const defaultTheme = createTheme();
 
 export default function Experience() {
-  const [jobEntries, setJobEntries] = useState([]);
-
-  useEffect(() => {
-    fetchJobEntries();
-  }, []);
-
-  const fetchJobEntries = async () => {
-    try {
-      const apiResponse = await API.graphql({ query: listJobEntries });
-      const entries = apiResponse.data.listJobEntries.items;
-      const sortedJobEntries = entries.sort((a, b) =>
-        b.targetDate.localeCompare(a.targetDate)
-      );
-      setJobEntries(sortedJobEntries);
-    } catch (error) {
-      console.error("Error fetching job entries:", error);
-    }
-  };
 
   return (
     <ThemeProvider theme={defaultTheme}>
@@ -65,22 +45,42 @@ export default function Experience() {
               boxSizing: "border-box",
             }}
           >
-            {jobEntries.map((entry) => (
-              <JobEntry
-                key={entry.id}
-                companyName={entry.companyName}
-                jobTitle={entry.jobTitle}
-                timeTracker={
-                  <TimeTracker
-                    startDate={
-                      entry.startDate == null ? new Date() : new Date(entry.startDate)
-                    }
-                    targetDate={new Date(entry.targetDate)}
-                  />
-                }
-                jobDescription={entry.jobDescription}
-              />
-            ))}
+            <JobEntry
+            companyName="CACI Federal"
+            jobTitle="Systems/Software Engineer T3"
+            timeTracker={<TimeTracker targetDate={new Date("2020-02-18")} />}
+            jobDescription={"Solicited new business with the USMC within the GCSS-MC program. The proof of concept supplied demonstrated the ability to submit in the field logistics tracking tasks while operating with no network connection. \n\n Successfully implemented a new claims module within a JSF logistics application. This included leading the integration of a file management feature for customers and government officials to submit, manage, and approve documentation throughout the lifecycle of a claim."}
+          />
+          <JobEntry
+            companyName="Enterprise Holdings, Inc."
+            jobTitle="Software Engineer II"
+            timeTracker={<TimeTracker startDate={new Date("2016-02-18")} targetDate={new Date("2020-02-17")} />}
+            jobDescription={"Implemented new features and resolved bugs within a portfolio of products in the Carshare department, such as API Gateway, Fleet Communicator, Renter Service Integration, Driver Qualification, and Rental Operations. \n\n Partook in multiple projects for the Daily Rental Operations department, including ECARS to Renter Service, ECARS Legacy Disconnect, PCI Compliance, Truck Telematics, and Central Bulk Payment."}
+          />
+          <JobEntry
+            companyName="CACI Federal"
+            jobTitle="Systems/Software Engineer T2"
+            timeTracker={<TimeTracker startDate={new Date("2015-05-01")} targetDate={new Date("2016-02-01")} />}
+            jobDescription="Helped maintain the DPS logistics application."
+          />
+          <JobEntry
+            companyName="Hudson Bay Company"
+            jobTitle="Associate Programmer Analyst"
+            timeTracker={<TimeTracker startDate={new Date("2014-07-01")} targetDate={new Date("2015-05-01")} />}
+            jobDescription="Maintained the e-commerce platform Sterling OMS and took on release manager responsibilities for customizations to the product. Also provided as-needed production support."
+          />
+          <JobEntry
+            companyName="DPRA"
+            jobTitle="Java Developer"
+            timeTracker={<TimeTracker startDate={new Date("2014-01-01")} targetDate={new Date("2014-07-01")} />}
+            jobDescription="Ensured a smooth transition from an original federal contractor to newly awarded CACI for the DPS program. Assisted in standing up infrastructure technologies while providing as-needed analysis for production support issues."
+          />
+          <JobEntry
+            companyName="Savvis"
+            jobTitle="Associate Engineer"
+            timeTracker={<TimeTracker startDate={new Date("2012-12-01")} targetDate={new Date("2013-12-01")} />}
+            jobDescription="Provided junior-level system administration support for clients using the company's cloud computing and hosting services. Helped support multiple Fortune 500 companies accomplish their goals and meet established SLA expectations."
+          />
           </Box>
         </Box>
       </main>
